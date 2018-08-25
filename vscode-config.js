cc760249c00722d338e41d15ecc62a9249bde9ea
CHANN <chann@chann.kr>
2018-08-25 22:19:48 +0900

    Init: VS Code Config - Mainly use SettingSync. This is secondary backup.

---
+++ vscode-config.js
@@ -0,0 +1,65 @@
+{
+    "[cpp]": {
+        "editor.wordWrap": "on",
+    },
+    "[javascript]": {
+        "editor.tabSize": 2
+    },
+    "[markdown]": {
+        "editor.wordWrap": "bounded",
+        "editor.wordWrapColumn": 100,
+        "editor.quickSuggestions": true,
+        "editor.tabSize": 2
+    },
+    "[scss]": {
+        "editor.tabSize": 2
+    },
+    "[typescript]": {
+        "editor.tabSize": 2
+    },
+    "breadcrumbs.enabled": true,
+    "css.lint.unknownProperties": "ignore",
+    "editor.fontFamily": "Bitstream Vera Sans Mono, Menlo, Monaco, 'Courier New', monospace",
+    "editor.fontSize": 14,
+    "editor.formatOnPaste": false,
+    "editor.formatOnSave": false,
+    "editor.minimap.enabled": true,
+    "editor.minimap.maxColumn": 100,
+    "editor.minimap.showSlider": "always",
+    "editor.multiCursorModifier": "ctrlCmd",
+    "editor.renderWhitespace": "all",
+    "editor.rulers": [120],
+    "editor.wordWrap": "bounded",
+    "editor.wordWrapColumn": 120,
+    "files.insertFinalNewline": true,
+    "files.trimTrailingWhitespace": true,
+    "git.autofetch": true,
+    "git.enableSmartCommit": true,
+    "html.format.wrapLineLength": 0,
+    "html.suggest.html5": true,
+    "markdown.preview.scrollEditorWithPreview": true,
+    "python.linting.pylintArgs": ["--disable=E1101,C0111,C0103", "--max-line-length=120" ],
+    "python.pythonPath": "/Users/chann/.pyenv/shims/python",
+    "scss.lint.unknownProperties": "ignore",
+    "search.smartCase": true,
+    "sync.askGistName": false,
+    "sync.autoDownload": false,
+    "sync.autoUpload": false,
+    "sync.forceDownload": false,
+    "sync.gist": "<SECRET>",
+    "sync.host": "",
+    "sync.lastDownload": "",
+    "sync.lastUpload": "2018-08-23T03:09:45.293Z",
+    "sync.pathPrefix": "",
+    "sync.quietSync": false,
+    "sync.removeExtensions": true,
+    "sync.syncExtensions": true,
+    "window.zoomLevel": 0,
+    "workbench.activityBar.visible": true,
+    "workbench.colorTheme": "One Dark Pro",
+    "workbench.sideBar.location": "left",
+    "workbench.startupEditor": "newUntitledFile",
+    "workbench.statusBar.feedback.visible": false,
+    "workbench.statusBar.visible": true,
+}
+
