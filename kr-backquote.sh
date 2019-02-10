b82b89d37774ca52c9839c2f143847ad6c2416ac
CHANN <chann@chann.kr>
2019-02-10 21:52:27 +0900

    Add: Change Backquote for Korean macOS Users

---
+++ kr-backquote.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "[ Change Backquote for Korean macOS Users ]"
+
+if [ -f ~/Library/KeyBindings/DefaultkeyBinding.dict ]; then
+	echo "~/Library/KeyBindings/DefaultkeyBinding.dict already exists"
+	exit -1
+fi
+
+mkdir -p ~/Library/KeyBindings
+cat << EOF > ~/Library/KeyBindings/DefaultkeyBinding.dict 
+{
+    "₩" = ("insertText:", "\`");
+}
+EOF
+
+echo "...Done!"
+
