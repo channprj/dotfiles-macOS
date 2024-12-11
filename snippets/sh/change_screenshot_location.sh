d8a9b0c10d8ae28a49ac46819246644527378f66
CHANN <chann@chann.kr>
2024-12-11 16:07:52 +0900

    update: use iCloud folder as default screenshot location

--- snippets/sh/change_screenshot_location.sh
+++ snippets/sh/change_screenshot_location.sh
@@ -1,2 +1,3 @@
-defaults write com.apple.screencapture location "~/Documents/Screenshots"
+# defaults write com.apple.screencapture location "~/Documents/Screenshots"
+defaults write com.apple.screencapture location $HOME/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/Screenshots
 
